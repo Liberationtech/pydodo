d29b6a31163d82b50d7bc030e1082ebc7ec73071
Oivvio Polite <oivvio@liberationtech.net>
2012-11-02 21:37:52 +0100

    initial import of ancient codebase

---
+++ setup.py
@@ -0,0 +1,23 @@
+from distutils.core import setup
+
+topic = """
+Development Status :: 1 - Planning
+Environment :: Console
+Intended Audience :: Other Audience
+License :: OSI Approved :: GNU Library or Lesser General Public License (LGPL)
+License :: OSI Approved :: MIT License
+Operating System :: OS Independent
+Programming Language :: Python
+Programming Language :: Python :: 3
+Topic :: Text Processing :: General
+"""
+
+setup(
+    name='pydodo',
+    packages=['python'],
+    version='0.0.1',
+    description='Markov chain generator',
+    author='Oivvio Polite',
+    author_email='oivvio@liberationtech.net',
+    topic=topic,
+)
